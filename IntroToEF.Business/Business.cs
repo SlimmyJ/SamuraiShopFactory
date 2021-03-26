95bc96f5373ba011518fd190fda10012f37facfa
Simon Van Tittelboom <simon.van.tittelboom@gmail.com>
2021-03-26 15:42:21 +0100

    1542

--- IntroToEF.Business/Business.cs
+++ IntroToEF.Business/Business.cs
@@ -2,29 +2,35 @@
 using IntroToEF.Data.Repositories;
 using System.Collections.Generic;
 using System;
+using System.Threading.Channels;
+using System.Xml;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace IntroToEF.Business
-{
-    public class Business
     {
+    public class
+        Business
+        {
         // Composition
         private ISamuraiRepo _repo;
 
         public Business()
-        {
+            {
             _repo = new SamuraiRepo();
-        }
+            }
 
         public void RunApp()
-        {
+            {
             Console.WriteLine("What?");
             Console.WriteLine("1. Create a new Samurai");
             Console.WriteLine("2. Add Horses to a Samurai");
+            Console.WriteLine("3. Show samurai info");
+            Console.WriteLine("4. See all samurais with horses");
 
             int userInput = Convert.ToInt32(Console.ReadLine());
 
             switch (userInput)
-            {
+                {
                 case 1:
                     CreateANewSamurai();
                     break;
@@ -36,66 +42,72 @@ namespace IntroToEF.Business
                     Console.WriteLine("Give a name of the Horse: ");
                     string name = Console.ReadLine();
                     sum.Horses.Add(new Horse
-                    {
+                        {
                         Name = name
-                    });
+                        });
 
                     _repo.UpdateSamurai(sum);
                     break;
+
+                case 3:
+                    Console.WriteLine("Enter the samurai's ID:");
+                    int userinput = Convert.ToInt32(Console.ReadLine());
+                    var thissamurai = FindSamuraiById(userinput);
+
+                    Console.WriteLine($"This samurais name is {thissamurai.Name}");
+                    Writehorses(thissamurai);
+
+                    break;
+
+                case 4:
+                    Console.WriteLine();
+                    break;
+
                 default:
                     break;
+                }
             }
-        }
+
+        private void Writehorses(Samurai thissamurai)
+            {
+            foreach (Horse thisguyshorses in thissamurai.Horses)
+                {
+                Console.WriteLine(thisguyshorses.Name);
+                }
+            }
 
         private void CreateANewSamurai()
-        {
+            {
             Console.WriteLine("Hello. Please enter a name for your Samurai:");
             string name = Console.ReadLine();
             Console.WriteLine("Dynasty of your Samurai");
             string dynasty = Console.ReadLine();
             _repo.AddSamurai(name, dynasty);
             Console.ReadLine();
-        }
+            }
 
         private Samurai FindSamuraiById(int id)
-        {
-            Samurai sum = _repo.GetSamurai(id);
+            {
+            Samurai sum = _repo.GetSamurai(id, true);
             Console.WriteLine($"Name: {sum.Name} Dynasty: {sum.Dynasty}");
             return sum;
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            }
 
         public List<Samurai> GetSamuraiWhoSaidAWord(string word)
-        {
-           var result = _repo.GetResultFromStoredProcedure(word);
+            {
+            var result = _repo.GetResultFromStoredProcedure(word);
             return result;
-        }
+            }
 
         public void RemoveSamurai(int id)
-        {
+            {
             _repo.DeleteSamurai(id);
-        }
+            }
 
         public void AddSamuraiWithHorses()
-        {
-            var samurai = new Samurai
             {
+            var samurai = new Samurai
+                {
                 Name = "Samurai With Horses",
                 Dynasty = "Sengoku",
                 Horses = new List<Horse>
@@ -111,15 +123,15 @@ namespace IntroToEF.Business
                         Name = "Boeddika"
                     }
                 }
-            };
+                };
 
             _repo.AddSamurai(samurai);
-        }
+            }
 
         public void AddSamuraiWhoFoughtInBattles()
-        {
-            Samurai veteran = new Samurai
             {
+            Samurai veteran = new Samurai
+                {
                 Name = "A weary broken man",
                 Battles = new List<Battle>
                 {
@@ -134,18 +146,18 @@ namespace IntroToEF.Business
                         Year = 2011
                     }
                 }
-            };
+                };
 
             _repo.AddSamurai(veteran);
-        }
+            }
 
         public void GetAllSamurais()
-        {
+            {
             var samurais = _repo.GetSamurais();
-        }
+            }
 
         public void RenameSamurai(int id, string name)
-        {
+            {
             // Get element from DB
             Samurai samuraiToBeUpdated = _repo.GetSamurai(id);
 
@@ -154,17 +166,17 @@ namespace IntroToEF.Business
 
             // Save object back to db
             _repo.UpdateSamurai(samuraiToBeUpdated);
-        }
+            }
 
         public void RenameMultipleSamurais()
-        {
+            {
             // Bad practice -> Code in datalayer should go here.
             _repo.UpdateSamurais();
-        }
+            }
 
         public Samurai GetSamuraiWithBattles(int id)
-        {
+            {
             return _repo.GetSamurai(id, true);
+            }
         }
     }
-}
--- IntroToEF.Data/Repositories/ISamuraiRepo.cs
+++ IntroToEF.Data/Repositories/ISamuraiRepo.cs
@@ -1,24 +1,39 @@
 ﻿using IntroToEF.Data.Entities;
+
 using System.Collections.Generic;
 
 namespace IntroToEF.Data.Repositories
-{
-    public interface ISamuraiRepo
     {
+    public interface ISamuraiRepo
+        {
         void AddDifferentObjectsToContext();
+
         void AddSamurai(Samurai samurai);
+
         void AddSamurai(string name, string dynasty);
+
         void AddSamurais(List<Samurai> samurais);
+
         void DeleteSamurai(int id);
+
         List<Samurai> GetResultFromStoredProcedure(string text);
-        Samurai GetSamurai(int id, bool fetchAllRelatedData = false);
+
+        Samurai GetSamurai(int id, bool fetchAllRelatedData = false); //CHECK THIS
+
         Samurai GetSamuraiByName(string name);
+
         List<Samurai> GetSamurais();
+
         List<Samurai> GetSamuraisByName(string name);
+
         List<Samurai> GetSamuraiWhereNameContains(string text);
+
         Samurai GetSamuraiWithIncludedData(int id);
+
         void GetSamuraiWithSql();
+
         void UpdateSamurai(Samurai samurai);
+
         void UpdateSamurais();
+        }
     }
-}
